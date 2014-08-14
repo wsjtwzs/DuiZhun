7fe60b5c816d9b6f3c7c83fb66c5a9eee9a2329f
evafan2003 <evafan2003@evafan2003-MacB.local>
2014-08-15 03:32:35 +0800

    fuck

--- DuiZhun/LoginView/RegViewController.m
+++ DuiZhun/LoginView/RegViewController.m
@@ -30,6 +30,17 @@
     
         _keyboardScrollView = self.scrollView;
         _keyboardScrollView.contentSize = CGSizeMake(CGRectGetWidth(_keyboardScrollView.frame), CGRectGetHeight(_keyboardScrollView.frame)+100);
+    
+    //给图标设置图片
+    self.user_icon.image = [IonIcons imageWithIcon:icon_ios7_person iconColor:[UIColor grayColor]
+                                          iconSize:20.0f
+                                         imageSize:CGSizeMake(20.0f, 20.0f)];
+    self.pass_icon.image = [IonIcons imageWithIcon:icon_ios7_unlocked iconColor:[UIColor grayColor]
+                                          iconSize:20.0f
+                                         imageSize:CGSizeMake(20.0f, 20.0f)];
+    //设置textfiled的字颜色
+    self.user_field.textColor = [UIColor whiteColor];
+    self.pass_field.textColor = [UIColor whiteColor];
 }
 
 - (void)didReceiveMemoryWarning
@@ -60,6 +71,51 @@
     [self.navigationController popToRootViewControllerAnimated:YES];
 }
 
+- (IBAction)user_start:(id)sender {
+    
+    [self changeUser];
+}
+
+- (IBAction)user_end:(id)sender {
+    [self done];
+    
+}
+
+- (IBAction)pass_start:(id)sender {
+    [self changePass];
+}
+
+- (IBAction)pass_end:(id)sender {
+    [self done];
+}
+
+-(void) changeUser {
+    self.user_icon.image = [IonIcons imageWithIcon:icon_ios7_person iconColor:[UIColor yellowColor]
+                                          iconSize:20.0f
+                                         imageSize:CGSizeMake(20.0f, 20.0f)];
+    self.pass_icon.image = [IonIcons imageWithIcon:icon_ios7_unlocked iconColor:[UIColor grayColor]
+                                          iconSize:20.0f
+                                         imageSize:CGSizeMake(20.0f, 20.0f)];
+}
+
+-(void) changePass {
+    self.user_icon.image = [IonIcons imageWithIcon:icon_ios7_person iconColor:[UIColor grayColor]
+                                          iconSize:20.0f
+                                         imageSize:CGSizeMake(20.0f, 20.0f)];
+    self.pass_icon.image = [IonIcons imageWithIcon:icon_ios7_unlocked iconColor:[UIColor yellowColor]
+                                          iconSize:20.0f
+                                         imageSize:CGSizeMake(20.0f, 20.0f)];
+}
+
+
+-(void) done {
+    self.user_icon.image = [IonIcons imageWithIcon:icon_ios7_person iconColor:[UIColor grayColor]
+                                          iconSize:20.0f
+                                         imageSize:CGSizeMake(20.0f, 20.0f)];
+    self.pass_icon.image = [IonIcons imageWithIcon:icon_ios7_unlocked iconColor:[UIColor grayColor]
+                                          iconSize:20.0f
+                                         imageSize:CGSizeMake(20.0f, 20.0f)];
+}
 
 
 @end
